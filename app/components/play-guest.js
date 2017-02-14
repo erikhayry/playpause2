2550b079547a41ab535e8e4c107e2e375cec60a6
Erik Portin <erikportin@gmail.com>
2017-02-14 21:51:52 +0100

    handle require!

--- app/components/play-guest.js
+++ app/components/play-guest.js
@@ -1,13 +1,14 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
     didInsertElement() {
-        const MAIN = require('electron').ipcRenderer;
+        const { ipcRenderer } = requireNode('electron');
+        console.log(ipcRenderer);
         let webview = this.element.getElementsByTagName('webview')[0];
         webview.addEventListener('did-stop-loading', () => {
             webview.openDevTools();
         });
         console.log(this.element.getElementsByTagName('webview')[0]);
-        MAIN.on('playpause', (event) => {
+        ipcRenderer.on('playpause', (event) => {
             console.log('playpause');
             console.log(event);
             webview.executeJavaScript('console.log("playpause")');
--- app/components/play-guest.ts
+++ app/components/play-guest.ts
@@ -6,14 +6,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     didInsertElement(){
-        const MAIN:IpcRenderer = require('electron').ipcRenderer;
+        const {ipcRenderer} = require('electron')
+        console.log(ipcRenderer)
         let webview = this.element.getElementsByTagName('webview')[0];
         webview.addEventListener('did-stop-loading', () => {
             webview.openDevTools();
         });
         console.log(this.element.getElementsByTagName('webview')[0])
 
-        MAIN.on('playpause', (event:IpcRendererEvent) => {
+        ipcRenderer.on('playpause', (event:IpcRendererEvent) => {
             console.log('playpause')
             console.log(event)
             webview.executeJavaScript('console.log("playpause")');

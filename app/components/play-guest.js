34f3ff6b99d8293a9819f578112ab41e019f490e
Erik Portin <erikportin@gmail.com>
2017-04-17 21:57:28 +0200

    active action class

--- app/components/play-guest.js
+++ app/components/play-guest.js
@@ -1,27 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    
+
     onWebviewReady(webview) {
         const { ipcRenderer } = require('electron');
 
         ipcRenderer.on('playpause', () => {
             console.log(this.get('stationPlayPauseAction'));
+            this.sendAction('activeAction', 'playpause');
             webview.executeJavaScript(this.get('stationPlayPauseAction'));
         });
 
         ipcRenderer.on('stop', () => {
             console.log(this.get('stationStopAction'));
+            this.sendAction('activeAction', 'stop');
             webview.executeJavaScript(this.get('stationStopAction'));
         });
 
         ipcRenderer.on('next', () => {
             console.log(this.get('stationNextAction'));
+            this.sendAction('activeAction', 'next');
             webview.executeJavaScript(this.get('stationNextAction'));
         });
 
         ipcRenderer.on('previous', () => {
             console.log(this.get('stationPreviousAction'));
+            this.sendAction('activeAction', 'previous');
             webview.executeJavaScript(this.get('stationPreviousAction'));
         });
 
--- app/controllers/station.js
+++ app/controllers/station.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    resetActiveActions: function(){
+        this.set('previousActionActive', false);
+        this.set('playPauseActionActive', false);
+        this.set('nextActionActive', false);
+        this.set('stopActionActive', false);
+    },
     actions: {
         removeStation: function(id){
             let that = this;
@@ -10,5 +16,27 @@ export default Ember.Controller.extend({
                 that.transitionToRoute('stations');
             });
         },
+        activeAction: function(currentAction){
+            console.log('activeAction', currentAction);
+            //TODO refactor
+            switch (currentAction){
+                case 'previous':
+                    this.set('previousActionActive', true);
+                    break;
+                case 'playpause':
+                    this.set('playPauseActionActive', true);
+                    break;
+                case 'next':
+                    this.set('nextActionActive', true);
+                    break;
+                case 'stop':
+                    this.set('stopActionActive', true);
+                    break;
+            }
+
+            Ember.run.later((function() {
+                this.resetActiveActions();
+            }).bind(this), 2000);
+        }
     }
 });
